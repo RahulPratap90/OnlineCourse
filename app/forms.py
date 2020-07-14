501af8a525a658e6faa4aecf40b6d1b1a52dd465
Your Name <rahulpratap601@gmail.com>
2020-07-14 08:50:40 +0530

    First commit

---
+++ app/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from app.models import Course_table  ,StudentEnrollCourse
+
+class Course_tableForm(forms.ModelForm):
+    class Meta:
+        model=Course_table
+        fields=['id','Course_Name','Faculty','class_date','class_time','Fee','Duration']
+
+
+class StudentEnrollCourseForm(forms.ModelForm):
+    class Meta:
+        model=StudentEnrollCourse
+        fields=['Name','phone']
